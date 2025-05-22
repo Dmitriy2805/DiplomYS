3b2993824cfa4c64b4daae57122d839ecc7031f7
Dmitriy2805 <dvovchinnikov2805@yandex.ru>
2025-05-22 16:06:21 +0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE="https://d1796eb6-47dd-4c8e-9e7f-5012a1c30063.serverhub.praktikum-services.ru"
-CREATE_ORDER="/api/v1/orders"
+URL_SERVICE = "https://d1796eb6-47dd-4c8e-9e7f-5012a1c30063.serverhub.praktikum-services.ru"
+CREATE_ORDER = "/api/v1/orders"
 TRACK_ORDER = "/api/v1/orders/track"
