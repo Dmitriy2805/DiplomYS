daccf27afea745f562f3dee3e0e075c140e1017e
Dmitriy2805 <dvovchinnikov2805@yandex.ru>
2025-05-22 16:06:42 +0500

    Update automation_test.py

--- automation_test.py
+++ automation_test.py
@@ -4,8 +4,9 @@ import sender_stand_request
 
 # Автоматизация:
 
+
 def test_get_order_info_by_track():
     request_result = sender_stand_request.create_order()
-    track_number=request_result.json()["track"]
+    track_number = request_result.json()["track"]
     request_result = sender_stand_request.get_order_info_by_track(track_number)
     assert request_result.status_code == 200
